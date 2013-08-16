331938f0ab474c959c0d655dc1c38396814a14a9
Kyle Robinson Young <kyle@dontkry.com>
2013-08-15 22:38:54 -0700

    Populate task description. Closes gruntjs/grunt-init#19

--- root/tasks/name.js
+++ root/tasks/name.js
@@ -13,7 +13,7 @@ module.exports = function(grunt) {
   // Please see the Grunt documentation for more information regarding task
   // creation: http://gruntjs.com/creating-tasks
 
-  grunt.registerMultiTask('{%= short_name %}', 'Your task description goes here.', function() {
+  grunt.registerMultiTask('{%= short_name %}', '{%= description %}', function() {
     // Merge task-specific and/or target-specific options with these defaults.
     var options = this.options({
       punctuation: '.',
